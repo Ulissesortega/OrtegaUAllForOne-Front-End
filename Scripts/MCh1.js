7635d377009d0aac7926e89e467caf0603c7ab52
Ulissesortega <51983586+Ulissesortega@users.noreply.github.com>
2023-01-11 09:39:46 -0800

    upload

---
+++ Scripts/MCh1.js
@@ -0,0 +1,45 @@
+
+
+
+
+
+
+// function urlCall(url) {
+//     fetch(url).then(
+//         response => response.text()
+//     ).then(data => 
+//         {
+//             helloReturn.textContent = data
+//             console.log(data)
+//         }
+//     )
+// }
+
+// function helloApi(nameInput){
+//     var letters = /^[A-Za-z]+$/;
+//     if(nameInput.value.match(letters)){
+//         savedInput = nameInput.value;
+//         sayHelloUrl = "https://ulisesallforone.azurewebsites.net/All4One/Hello/Ulises";
+//         console.log('Hello')
+//         urlCall(sayHelloUrl)
+//     }else{
+//         helloReturn.textContent = "Enter a valid response";
+//     }
+// }
+
+// helloApi('hello');
+
+
+// function helloApi(nameInput){
+//     var letters = /^[A-Za-z]+$/;
+//     if(nameInput.value.match(letters)){
+//         savedInput = nameInput.value;
+//         sayHelloUrl = "https://ulisesallforone.azurewebsites.net/All4One/Hello/Ulises";
+//         urlCall(sayHelloUrl)
+//     }else{
+//         helloReturn.textContent = "Enter a valid response";
+//     }
+// }
+
+// console.log("nameInput");
+// helloApi('hello');
