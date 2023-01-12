e1ceb998126c80ed980a7812b9195e8f3996a164
Ulissesortega <51983586+Ulissesortega@users.noreply.github.com>
2023-01-12 11:55:04 -0800

    upload

--- Scripts/MCh2.js
+++ Scripts/MCh2.js
@@ -1,9 +1,19 @@
-function Adding(){
-fetch('https://ulisesallforone.azurewebsites.net/All4One/adding/1/2')
-  .then((response) => response.text())
-  .then((data) => console.log(data));
-}
+let input1 = document.getElementById("input1");
+let input2 = document.getElementById("input2");
+let helloReturn = document.getElementById("helloReturn");
+let addingBtn = document.getElementById("addingBtn");
 
-Adding();
+
+addingBtn.addEventListener("click", function(){
+  GetData()
+})
+
+function GetData(){
+      fetch("https://ulisesallforone.azurewebsites.net/All4One/adding/" + input1.value + "/" + input2.value).then(
+      response => response.text()).then(
+        data => helloReturn.textContent = data
+      )
+    }
+  
 
 
--- Scripts/MCh3.js
+++ Scripts/MCh3.js
@@ -1,10 +1,25 @@
-function Asking(){
-  fetch('https://ulisesallforone.azurewebsites.net/All4One/questions/Ulises/6:30 AM')
-    .then((response) => response.text())
-    .then((data) => console.log(data));
-  }
+// function Asking(){
+//   fetch('https://ulisesallforone.azurewebsites.net/All4One/questions/Ulises/6:30 AM')
+//     .then((response) => response.text())
+//     .then((data) => console.log(data));
+//   }
 
-Asking();
+let input1 = document.getElementById("input1");
+let input2 = document.getElementById("input2");
+let helloReturn = document.getElementById("helloReturn");
+let addingBtn = document.getElementById("addingBtn");
 
+
+addingBtn.addEventListener("click", function(){
+  GetData()
+})
+
+function GetData(){
+      fetch("https://ulisesallforone.azurewebsites.net/All4One/questions/" + input1.value + "/" + input2.value).then(
+      response => response.text()).then(
+        data => helloReturn.textContent = data
+      )
+    }
+  
 
 
